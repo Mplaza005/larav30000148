497a018f04dac03000bbc14dcc62b7bf3662131e
Manuel Alejandro <mplaza005@gmail.com>
2025-04-25 10:05:13 -0500

    act

--- app/Http/Controllers/OrmController.php
+++ app/Http/Controllers/OrmController.php
@@ -14,21 +14,18 @@ class OrmController extends Controller
     public function consultas()
     {
 
+    //      $user = User::find(2);
+    //      return $user->profiles;//$user->posts;$user->roles;
 
-        //  $user = User::find(2);
-        //  return $user->profiles;//$user->posts;$user->roles;
+    //     $profile = Profile::find(2);
+    //     return $profile->user;
 
-        // $profile = Profile::find(2);
-        // return $profile->user;
+    //      $post=Post::find(3);
+    //      return $post->category;
 
-        //  $post=Post::find(3);
-        //  return $post->category;
+    //    // RELACIONES ANIDADAS
 
-        //RELACIONES ANIDADAS
-
-        //return User::with('posts.category')->get();
-
-
+    //     return User::with('posts.category')->get();
 
         //  $role = Role::find(3);
         //  $role->users()->attach(2);
